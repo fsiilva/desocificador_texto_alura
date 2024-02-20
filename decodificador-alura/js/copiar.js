69737d038a26c7e2083c0a92deb42304f024d985
fsiilva <111534447+fsiilva@users.noreply.github.com>
2024-02-20 10:27:43 -0300

    Delete decodificador-alura directory

--- decodificador-alura/js/copiar.js
+++
@@ -1,9 +0,0 @@
-function copiarTexto() {
-    let textoCopiado = document.querySelector(".texto-criptografado p");
-    var btnCopy = document.querySelector("#copiar");
-    btnCopy.addEventListener("click", function () {
-        navigator.clipboard.writeText(textoCopiado.innerText);
-        alert("Texto copiado");
-        location.reload();
-    })
-}
--- decodificador-alura/js/text-area.js
+++
@@ -1,7 +0,0 @@
-let textArea = document.querySelector("#texto-principal");
-textArea.addEventListener('input', autoResize, false);
-
-function autoResize() {
-    this.style.height = 'auto';
-    this.style.height = this.scrollHeight + 'px';
-}
